b2de77c5bea62818b92b6b7f4ffd496bdf102107
LautyT02 <lautyt02@gmail.com>
2023-09-06 19:50:10 -0300

    Last commit with Mostrar/Ocultar todos los Proyectos

--- js/main.js
+++ js/main.js
@@ -1,66 +1,77 @@
-//clase Proyecto
-// class Proyecto {
-//     constructor(id, nombre, descripcion, lenguajes, categorias, linkGitHub, linkGitLab){
-//         this.id=id
-//         this.nombre=nombre
-//         this.descripcion=descripcion
-//         this.lenguajes=lenguajes
-//         this.categorias=categorias
-//         this.linkGitHub=linkGitHub
-//         this.linkGitLab=linkGitLab
-//     }
-//     imprimirProyecto(){
-        // let listaLenguajes="Lenguajes utilizados: "
-        // for(let i=0;i<this.lenguajes.length;i++){
-        //     listaLenguajes =listaLenguajes+this.lenguajes[i]+"; "
-        // }
-        // let listaCategorias="Categorias: "
-        // for(let i=0;i<this.categorias.length;i++){
-        //     listaCategorias+=`${this.categorias[i]}; `
-        // }
-//         return `${this.nombre}\n ${this.descripcion}\n id: ${this.id}\n Lenguajes utilizados: ${this.lenguajes}\n Categorias: ${this.categorias}\n link a GitHub: ${this.linkGitHub}\n link a GitLab: ${this.linkGitLab}\n\n`
-//     }
-// }
-
-//Index
+// clase Proyecto
+class Proyecto {
+    constructor(id, nombre, descripcion, lenguajes, categorias, linkGitHub, linkGitLab) {
+        this.id = id
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.lenguajes = lenguajes
+        this.categorias = categorias
+        this.linkGitHub = linkGitHub
+        this.linkGitLab = linkGitLab
+    }
+    imprimirProyecto() {
+        let listaLenguajes = "Lenguajes utilizados: "
+        for (let i = 0; i < this.lenguajes.length; i++) {
+            listaLenguajes = listaLenguajes + this.lenguajes[i] + "; "
+        }
+        let listaCategorias = "Categorias: "
+        for (let i = 0; i < this.categorias.length; i++) {
+            listaCategorias += `${this.categorias[i]}; `
+        }
+        return `${this.nombre}\n ${this.descripcion}\n id: ${this.id}\n Lenguajes utilizados: ${this.lenguajes}\n Categorias: ${this.categorias}\n link a GitHub: ${this.linkGitHub}\n link a GitLab: ${this.linkGitLab}\n\n`
+    }
+}
 
 //Traer Elementos del DOM
 //Botones
-let btnEliminarTodosP=document.getElementById("btn-el-todo")
-let btnEliminarUltP=document.getElementById("btn-el-ult")
-let btnMostrarOcultarP=document.getElementById("btn-most-ocul")
+let btnCrearP = document.getElementById("btn-crear-proyecto")
+let btnEliminarTodosP = document.getElementById("btn-el-todo")
+let btnEliminarUltP = document.getElementById("btn-el-ult")
+let btnMostrarOcultarP = document.getElementById("btn-most-ocul")
 // Sección Mensajes
-let tituloMensaje=document.getElementById("titulo-mensaje")
-let contenidoMensaje=document.getElementById("contenido-mensaje")
+let tituloMensaje = document.getElementById("titulo-mensaje")
+let contenidoMensaje = document.getElementById("contenido-mensaje")
 // Sección de Filtrado
-let radFind=document.getElementById("find-rad-id")
-let radFilter=document.getElementById("filter-rad-id")
-let searchBar=document.getElementById("search-id")
+let seccionFiltrado = document.querySelector(".filtrado")
+let radFind = document.getElementById("find-rad-id")
+let radFilter = document.getElementById("filter-rad-id")
+let searchBar = document.getElementById("search-id")
 //Sección Proyectos
-let seccionProyectos=document.querySelector(".proyectos")
-
-// Event Listeners
-// Botones
-btnEliminarTodosP.addEventListener("click",eliminarTodos)
-btnEliminarUltP.addEventListener("click", eliminarUltimo)
-btnMostrarOcultarP.addEventListener("click", mostrarOcultar)
-// Search Bar
-searchBar.addEventListener("input", buscar)
-
-//Pagina de Crear Proyecto
-//Traer Elementos del DOM
-let formulario=document.getElementById("formulario-crear-proyecto")
+let seccionProyectos = document.querySelector(".proyectos")
+let tituloProyectos = document.querySelector("#titulo-proyectos")
+//Formulario Creación Proyecto
+let seccionFormulario = document.querySelector(".formulario-proyecto")
+let btnForm = document.getElementById("btn-form-id")
 // let inputNombre=document.getElementById("nombre-id")
 // let inputDescripcion=document.getElementById("descripcion-id")
 // let checkLenguajes=document.getElementsByName("lenguajes")
 // let checkCategorias=document.getElementsByName("categorias")
-// let enlaceGitHub=getElementById("github-link-id")
-// let enlaceGitLab=getElementById("gitlab-link-id")
+// let enlaceGitHub=document.getElementById("github-link-id")
+// let enlaceGitLab=document.getElementById("gitlab-link-id")
 
 // Event Listeners
-formulario.addEventListener("submit", anadirProyecto)
+// Botones
+btnCrearP.addEventListener("click", anadirProyecto)
+btnEliminarTodosP.addEventListener("click", eliminarTodos)
+btnEliminarUltP.addEventListener("click", eliminarUltimo)
+btnMostrarOcultarP.addEventListener("click", mostrarOcultar)
+// Search Bar
+searchBar.addEventListener("input", buscar)
+// Formulario
+btnForm.addEventListener("click", subirProyecto)
 
 // Definición de variables
+let listaProyectos = [new Proyecto(0, "proyecto-1", "proyecto de prueba 1", "html, css, javascript", "pagina web", "https://github.com/lautyt02", "https://gitlab.com/lautyt02"),
+new Proyecto(1, "proyecto-2", "proyecto de prueba 2", "html, css, javascript", "pagina web", "https://github.com/lautyt02", "https://gitlab.com/lautyt02"),
+new Proyecto(2, "proyecto-3", "proyecto de prueba 3", "rust", "aplicación, criptomonedas", "https://github.com/lautyt02", "https://gitlab.com/lautyt02"),]
+
+
+
+
+
+
+
+
 // let listaProyectos = [new Proyecto(0,"proyecto-1","proyecto de prueba 1", "html, css, javascript","pagina web","https://github.com/lautyt02","https://gitlab.com/lautyt02" ),
 // new Proyecto(1,"proyecto-2","proyecto de prueba 2", "html, css, javascript","pagina web","https://github.com/lautyt02","https://gitlab.com/lautyt02" ),
 // new Proyecto(2,"proyecto-3","proyecto de prueba 3", "rust","aplicación, criptomonedas","https://github.com/lautyt02","https://gitlab.com/lautyt02" ),]
@@ -165,27 +176,95 @@ formulario.addEventListener("submit", anadirProyecto)
 //     return textoAimprimir
 // }
 
-function eliminarTodos(){
-        tituloMensaje.innerText="Mensaje de Prueba"
-    contenidoMensaje.innerText=`Eliminar Todos`
+function eliminarTodos() {
+    tituloMensaje.innerText = "Mensaje de Prueba"
+    contenidoMensaje.innerText = `Eliminar Todos`
 }
-function eliminarUltimo(){
-    tituloMensaje.innerText="Mensaje de Prueba"
-    contenidoMensaje.innerText=`Eliminar Último`
+function eliminarUltimo() {
+    tituloMensaje.innerText = "Mensaje de Prueba"
+    contenidoMensaje.innerText = `Eliminar Último`
 }
-function mostrarOcultar(){
-    tituloMensaje.innerText="Mensaje de Prueba"
-    contenidoMensaje.innerText=`Mostrar/Ocultar`
-    
-}
-function buscar(){
-    let radios=document.getElementsByName("find/filter")
-    // let radioSeleccionado=document.querySelectorAll('input[name="find/filter"]:checked').value
+function mostrarOcultar() {
 
-    tituloMensaje.innerText="Mensaje de Prueba"
-    contenidoMensaje.innerText=`Estas Tipeando \n Estado Find ${radios[0].checked} \n Estado Filter ${radios[1].checked}`
+    tituloMensaje.innerText = "Mensaje de Prueba"
+    contenidoMensaje.innerText = `Mostrar/Ocultar`
+
 }
-function anadirProyecto(){
-    let contenedor=document.querySelector(".formulario-proyecto")
-    contenedor.innerText=`Se añadio proyecto`
+function buscar() {
+    let radios = document.getElementsByName("find/filter")
+    if (radios[0].checked) {
+        encontrarNombre(listaProyectos, searchBar.value)
+    }
+    else {
+        filtrar(listaProyectos, "nombre", searchBar.value)
+    }
+
+    // tituloMensaje.innerText = "Mensaje de Prueba"
+    // contenidoMensaje.innerText = `Estas Tipeando ${searchBar.value} \n Estado Find ${radios[0].checked} \n Estado Filter ${radios[1].checked}`
+
+}
+function filtrar(arreglo, atributo, valor) {
+    tituloMensaje.innerText = "Ejecutando Filter"
+    contenidoMensaje.innerText = "Devuelve todos los resultados que contengan el valor del input."
+    let arregloFlitrado = arreglo.filter((el) => el[atributo].includes(valor))
+    if (arregloFlitrado) {
+        seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Ningún proyecto coincide con los criterios de Búsqueda</h2>`
+    }
+    else {
+        seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Los Proyectos que coinciden con los criterios de filtrado son:</h2>`
+        imprimir(arregloFlitrado)
+    }
+}
+function encontrarNombre(arreglo, nombreBuscado) {
+    tituloMensaje.innerText = "Ejecutando Find"
+    contenidoMensaje.innerText = "El imput debe ser exactamente igual. Devuelve un único resultado"
+    let resultadoBusqueda = arreglo.find((el) => el.nombre === nombreBuscado)
+    console.log(typeof resultadoBusqueda)
+    if (typeof resultadoBusqueda === "undefined") {
+        seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Ningún proyecto coincide con los criterios de Búsqueda</h2>`
+    }
+    else {
+        seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Resultado de Búsqueda:</h2>`
+        imprimir([resultadoBusqueda])
+    }
+}
+function anadirProyecto() {
+    tituloMensaje.innerText = "Mensaje de Prueba"
+    contenidoMensaje.innerText = `Crear Proyecto`
+    if (seccionFormulario.classList.contains("invisible")) {
+        seccionFormulario.classList.remove("invisible")
+        seccionProyectos.classList.add("invisible")
+        seccionFiltrado.classList.add("invisible")
+    }
+
+}
+function subirProyecto() {
+    seccionFormulario.classList.add("invisible")
+    seccionProyectos.classList.remove("invisible")
+    seccionFiltrado.classList.remove("invisible")
+
+}
+function guardarLS() {
+    for (const proyecto of listaProyectos) {
+        let proyectoJson = JSON.stringify(proyecto)
+        localStorage.setItem(proyecto.id, proyectoJson)
+    }
+}
+function imprimir(arregloProyectos) {
+    for (const proyecto of arregloProyectos) {
+        let proyectoHtml = document.createElement("div")
+        proyectoHtml.classList.add("tarjeta-proyecto")
+        proyectoHtml.innerHTML = `<h4>${proyecto.nombre}</h4>
+        <p>${proyecto.descripcion}</p>
+        <p>Lenguajes utilizados: ${proyecto.lenguajes}</p>
+        <a href="${proyecto.linkGitHub}" target="_blank">Repositorio GitHub</a>
+        <a href="${proyecto.linkGitLab}" target="_blank">Repositorio GitLab</a>`
+        seccionProyectos.appendChild(proyectoHtml)
+    }
+}
+function obtenerLS() {
+    for (let i = 0; i < localStorage.length; i++) {
+        listaProyectos[i] = JSON.parse(localStorage.getItem(i))
+        console.log(listaProyectos[i])
+    }
 }
