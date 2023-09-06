fecde94d9ccb1d0fc8f2299c192c58323d85bb45
LautyT02 <lautyt02@gmail.com>
2023-09-06 20:59:18 -0300

    Finished first version of PreEntrega3

--- js/main.js
+++ js/main.js
@@ -40,13 +40,14 @@ let seccionProyectos = document.querySelector(".proyectos")
 let tituloProyectos = document.querySelector("#titulo-proyectos")
 //Formulario Creación Proyecto
 let seccionFormulario = document.querySelector(".formulario-proyecto")
+let formulario = document.getElementById("formulario-creacion-proyecto")
 let btnForm = document.getElementById("btn-form-id")
-// let inputNombre=document.getElementById("nombre-id")
-// let inputDescripcion=document.getElementById("descripcion-id")
-// let checkLenguajes=document.getElementsByName("lenguajes")
-// let checkCategorias=document.getElementsByName("categorias")
-// let enlaceGitHub=document.getElementById("github-link-id")
-// let enlaceGitLab=document.getElementById("gitlab-link-id")
+let inputNombre=document.getElementById("nombre-id")
+let inputDescripcion=document.getElementById("descripcion-id")
+let checkLenguajes=document.getElementsByName("lenguajes")
+let checkCategorias=document.getElementsByName("categorias")
+let enlaceGitHub=document.getElementById("github-link-id")
+let enlaceGitLab=document.getElementById("gitlab-link-id")
 
 // Event Listeners
 // Botones
@@ -59,128 +60,29 @@ searchBar.addEventListener("input", buscar)
 btnForm.addEventListener("click", subirProyecto)
 
 // Definición de variables
-let listaProyectos = [new Proyecto(0, "proyecto-1", "proyecto de prueba 1", "html, css, javascript", "pagina web", "https://github.com/lautyt02", "https://gitlab.com/lautyt02"),
-new Proyecto(1, "proyecto-2", "proyecto de prueba 2", "html, css, javascript", "pagina web", "https://github.com/lautyt02", "https://gitlab.com/lautyt02"),
-new Proyecto(2, "proyecto-3", "proyecto de prueba 3", "rust", "aplicación, criptomonedas", "https://github.com/lautyt02", "https://gitlab.com/lautyt02"),]
+let listaProyectos = []
 
+// Inicio del programa
+obtenerLS()
+imprimir(listaProyectos)
 
-
-
-
-
-
-
-// let listaProyectos = [new Proyecto(0,"proyecto-1","proyecto de prueba 1", "html, css, javascript","pagina web","https://github.com/lautyt02","https://gitlab.com/lautyt02" ),
-// new Proyecto(1,"proyecto-2","proyecto de prueba 2", "html, css, javascript","pagina web","https://github.com/lautyt02","https://gitlab.com/lautyt02" ),
-// new Proyecto(2,"proyecto-3","proyecto de prueba 3", "rust","aplicación, criptomonedas","https://github.com/lautyt02","https://gitlab.com/lautyt02" ),]
-// Inicio programa
-// let ejecutar = true
-// while(ejecutar){
-//     let opcion = validarInputNumero(`En el siguiente promt, ingrese una de las siguientes opciones: \n
-//     \t 1 Filtrado \t 2 Busqueda por Nombre \n
-//     \t 3 para añadir un proyecto \n
-//     \t 5 Listar todos los Proyectos \t 6 Finalizar Ejecucuión`)
-//     switch (opcion){
-//         case 1:
-//             let atributoFiltro = ingresarTexto("Ingrese el atributo por el que desea filtrar")
-//             let valorFiltro = ingresarTexto("Ingrese el valor del atributo por el que desea filtrar")
-//             filtrar(listaProyectos,atributoFiltro,valorFiltro)
-//             break
-//         case 2:
-//             let nombreAbuscar = ingresarTexto("Ingrese el nombre del proyecto que busca:")
-//             buscarNombre(listaProyectos,nombreAbuscar)
-//             break
-//         case 3:
-//             anadirProyecto(listaProyectos)
-//             break
-//         case 5:
-//             console.log(imprimirTodos(listaProyectos))
-//             break
-//         case 6:
-//             ejecutar = false
-//             break
-//         default:
-//             console.log(`No existe acción para el número ${opcion}, intente de nuevo`)
-//             break
-//     }
-// }
-// Definición de funciones
-// function validarInputNumero(texto) {
-//     let valorValido=false
-//     while(!valorValido){
-//         let numero=prompt(texto)
-//         let condicion = !isNaN(numero) //Chequear que sea un número
-//         condicion = condicion && ((numero | 0) == numero) //chequear que sea entero
-//         condicion = condicion  && (numero>0)  //Chequear que sea positivo
-//         if(condicion){ //Chequear las 3 condiciones
-//             valorValido=true
-//             return parseInt(numero)
-//         }
-//         else {
-//             console.log("Debe ingresar un número entero positivo, intente de nuevo")
-//         }
-//     } 
-// }
-// function ingresarTexto(texto) {
-//     let valorValido=false
-//     while(!valorValido){
-//         textoIngresado = prompt(texto)
-//         condicionUno= typeof textoIngresado === 'string'
-//         condicionDos = textoIngresado instanceof String
-//         if (condicionUno || condicionDos){
-//             valorValido = true
-//             return textoIngresado.toLowerCase()
-//         }
-//         else{
-//             console.log("Debe ingresar un Texto, intente de nuevo")
-//         }
-//     }
-
-// }
-// function filtrar(arreglo, atributo, valor){
-//     let arregloFlitrado = arreglo.filter((el)=>el[atributo].includes(valor))
-//     textoAimprimir = `Los Proyectos que coinciden con los criterios de filtrado son: \n`
-//     for(let i=0;i<arregloFlitrado.length;i++){
-//         textoAimprimir += arregloFlitrado[i].imprimirProyecto()
-//     }
-//     console.log(textoAimprimir)
-// }
-// function buscarNombre(arreglo, nombreBuscado){
-//     let resultadoBusqueda = arreglo.find((el)=>el.nombre === nombreBuscado) 
-//     if(typeof resultadoBusqueda === "undefined"){
-//         console.log("Ningún proyecto coincido con los criterios de Búsqueda")
-//     }
-//     else{
-//         let textoAimprimir = `Resultado de Búsqueda:  \n`+resultadoBusqueda.imprimirProyecto()
-//         console.log(textoAimprimir)
-//     }
-// }
-// function anadirProyecto(arreglo){
-//     let id = arreglo[arreglo.length-1].id+1
-//     let nombre = ingresarTexto("Ingrese el Nombre del Proyecto")
-//     let descripcion = ingresarTexto("Ingrese una descripción para el Proyecto")
-//     let lenguajes="rust"
-//     let categorias="aplicación"
-//     let linkGitHub="https://github.com/lautyt02"
-//     let linkGitLab="https://gitlab.com/lautyt02"
-//     arreglo.push(new Proyecto(id, nombre, descripcion, lenguajes, categorias, linkGitHub, linkGitLab))
-// }
-
-// function imprimirTodos(arreglo){
-//     textoAimprimir = `Lista de Proyectos: \n`
-//     for(let i=0;i<arreglo.length;i++){
-//         textoAimprimir += arreglo[i].imprimirProyecto()
-//     }
-//     return textoAimprimir
-// }
-
+// Definicion de Funciones
 function eliminarTodos() {
-    tituloMensaje.innerText = "Mensaje de Prueba"
-    contenidoMensaje.innerText = `Eliminar Todos`
+    listaProyectos=[]
+    localStorage.clear()
+    tituloMensaje.innerText = "Todos los Proyectos Fueron Eliminados"
+    contenidoMensaje.innerText = ""
+    imprimir(listaProyectos)
 }
 function eliminarUltimo() {
-    tituloMensaje.innerText = "Mensaje de Prueba"
-    contenidoMensaje.innerText = `Eliminar Último`
+    localStorage.clear()
+    listaProyectos.pop()
+    guardarLS()
+    
+    seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Lista de Proyectos</h2>`
+    tituloMensaje.innerText = "Se Elimino El Último Proyecto"
+    contenidoMensaje.innerText = ""
+    imprimir(listaProyectos)
 }
 
 function buscar() {
@@ -200,7 +102,7 @@ function filtrar(arreglo, atributo, valor) {
     tituloMensaje.innerText = "Ejecutando Filter"
     contenidoMensaje.innerText = "Devuelve todos los resultados que contengan el valor del input."
     let arregloFlitrado = arreglo.filter((el) => el[atributo].includes(valor))
-    if (arregloFlitrado) {
+    if (arregloFlitrado.length==0) {
         seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Ningún proyecto coincide con los criterios de Búsqueda</h2>`
     }
     else {
@@ -212,7 +114,6 @@ function encontrarNombre(arreglo, nombreBuscado) {
     tituloMensaje.innerText = "Ejecutando Find"
     contenidoMensaje.innerText = "El imput debe ser exactamente igual. Devuelve un único resultado"
     let resultadoBusqueda = arreglo.find((el) => el.nombre === nombreBuscado)
-    console.log(typeof resultadoBusqueda)
     if (typeof resultadoBusqueda === "undefined") {
         seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Ningún proyecto coincide con los criterios de Búsqueda</h2>`
     }
@@ -222,20 +123,43 @@ function encontrarNombre(arreglo, nombreBuscado) {
     }
 }
 function anadirProyecto() {
-    tituloMensaje.innerText = "Mensaje de Prueba"
-    contenidoMensaje.innerText = `Crear Proyecto`
+    tituloMensaje.innerText = "Creando Proyecto"
+    contenidoMensaje.innerText = `Complete el formulario y haga click en "Agregar Proyecto"`
     if (seccionFormulario.classList.contains("invisible")) {
         seccionFormulario.classList.remove("invisible")
         seccionProyectos.classList.add("invisible")
         seccionFiltrado.classList.add("invisible")
     }
-
 }
 function subirProyecto() {
+    let id = 0
+    if(listaProyectos.length!=0){
+        id=listaProyectos[listaProyectos.length-1].id+1
+    }
+    let nombre = inputNombre.value
+    let descripcion = inputDescripcion.value
+    let lenguajes =[]
+    for(lenguaje of checkLenguajes){
+        if(lenguaje.checked){
+            lenguajes.push(lenguaje.value)
+        }
+    }
+    let categorias = []
+    for(categoria of checkCategorias){
+        if(categoria.checked){
+            categorias.push(categoria.value)
+        }
+    }
+    let linkGitHub = enlaceGitHub.value
+    let linkGitLab = enlaceGitLab.value
+    nuevoProyecto= new Proyecto(id,nombre,descripcion,lenguajes,categorias,linkGitHub,linkGitLab)
+    listaProyectos.push(nuevoProyecto)
+    guardarLS()
+    seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">Lista de Proyectos</h2>`
+    imprimir(listaProyectos)
     seccionFormulario.classList.add("invisible")
     seccionProyectos.classList.remove("invisible")
     seccionFiltrado.classList.remove("invisible")
-
 }
 function guardarLS() {
     for (const proyecto of listaProyectos) {
@@ -244,6 +168,9 @@ function guardarLS() {
     }
 }
 function imprimir(arregloProyectos) {
+    if(arregloProyectos.length==0){
+        seccionProyectos.innerHTML = `<h2 id="titulo-proyectos">No hay ningún proyecto guardado</h2>`
+    }
     for (const proyecto of arregloProyectos) {
         let proyectoHtml = document.createElement("div")
         proyectoHtml.classList.add("tarjeta-proyecto")
@@ -258,6 +185,5 @@ function imprimir(arregloProyectos) {
 function obtenerLS() {
     for (let i = 0; i < localStorage.length; i++) {
         listaProyectos[i] = JSON.parse(localStorage.getItem(i))
-        console.log(listaProyectos[i])
     }
 }
